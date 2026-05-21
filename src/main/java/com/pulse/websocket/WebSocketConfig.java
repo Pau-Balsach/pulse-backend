eb77a5254e9cbf5f4410ef6e8bdcfb53a1a9ccdd
pbalsach <sigmasogma335@gmail.com>
2026-05-21 05:04:40 +0200

    Render fix

--- src/main/java/com/pulse/websocket/WebSocketConfig.java
+++ src/main/java/com/pulse/websocket/WebSocketConfig.java
@@ -19,7 +19,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000")
+                .setAllowedOriginPatterns(
+                        "http://localhost:3000",
+                        "https://pulse-frontend-chi-three.vercel.app"
+                )
                 .withSockJS();
     }
 }
